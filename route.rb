5d83ba23135c896525e7909137c76782fb05774a
M0EKA <momoseaoi.1810@gmail.com>
2022-05-13 15:32:55 +0900

    erbの評価で複数行表示する

--- route.rb
+++ route.rb
@@ -1,11 +1,33 @@
 require 'bundler'
 require 'sinatra/reloader'
+require 'erb'
 Bundler.require
 
 get '/' do
-  'Hello world!'
+  erb :index
 end
 
 get '/about' do
   'About!!'
 end
+
+__END__
+
+@@index
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <%= @title %>
+  <p>Hello wolrd!</p>
+  <p>Hello Sinatra!</p>
+</body>
+
+</html>
